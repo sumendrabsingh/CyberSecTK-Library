1e65377cdd85713a9382429cfd69279310b704a0
sumendrabsingh <sumendrabsingh@users.noreply.github.com>
2019-12-27 11:02:09 -0600

    TCP IOT
    Library update for TCP

--- cybersectk/cybersectk/utilities.py
+++ cybersectk/cybersectk/utilities.py
@@ -1,4 +1,9 @@
+import os
+import glob
 from scapy.all import * 
+
+###################################################
+#################### WLAN IOT #####################
 f = open ("IOTwireless.csv", "w")
 f.writelines("version,Pad,Len,Rate,ChannelFrequency,ChannelFlags,dBm_AntSignal,Antenna,subtype,\
 type,proto,FCfield,ID,addr1,addr2,addr3,SC,addr4,Dot11Elt1.ID,Dot11Elt1.len,Dot11Elt1.info\n")
@@ -13,3 +18,53 @@ def wiot(frame):
                 f.writelines('%s,%s,%s,'%(frame.payload.ID,frame.payload.len,frame.info.decode()))
             f.writelines('\n')
 sniff(offline=input("Enter the Pcap file:"), prn=wiot)
+#######################################################
+#################### TCP IOT ##########################
+ip_filter = {} # python dictionary
+    
+ip_filter['TCP_Mobile'] = "'tcp && (ip.src==192.168.1.45)'"
+ip_filter['TCP_Outlet'] = "'tcp && (ip.src==192.168.1.222) || \
+                                        (ip.src==192.168.1.67)'"
+ip_filter['TCP_Assistant'] = "'tcp && (ip.src==192.168.1.111) || \
+                    (ip.src==192.168.1.30) || (ip.src==192.168.1.42) \
+                 || (ip.src==192.168.1.59) || (ip.src==192.168.1.70)'"
+ip_filter['TCP_Camera'] = "'tcp && (ip.src==192.168.1.128) || \
+                    (ip.src==192.168.1.145) || (ip.src==192.168.1.78)'"
+ip_filter['TCP_Miscellaneous'] = "'tcp && (ip.src==192.168.1.216) \
+                  || (ip.src==192.168.1.46) || (ip.src==192.168.1.84) \
+                     || (ip.src==192.168.1.91)'"
+
+#############################################################
+
+labelFeature = open("label_feature_IOT.csv",'a') #vector space
+
+labelFeature.writelines("Label,IPLength,IPHeaderLength,TTL,\
+           Protocol,SourcePort,DestPort,SequenceNumber,AckNumber\
+           ,WindowSize,TCPHeaderLength,TCPLength,TCPStream\
+     ,TCPUrgentPointer,IPFlags,IPID,IPchecksum,TCPflags,TCPChecksum\n")
+
+##############################################################
+def iot (**ip_filter):
+    for original in glob.glob('./*.pcap'):
+        for k in ip_filter.keys():
+            os.system("tshark -r " + original + " -w- -Y " + 
+                      ip_filter[k] + ">> filtered_pcap/" + k + ".pcap")
+
+#################################################################
+for filteredFile in glob.glob('filtered_pcap/*.pcap'):
+    #print(filteredFile)
+    filename = filteredFile.split('/')[-1]
+    label = filename.replace('.pcap', '')
+    tsharkCommand = "tshark -r " + filteredFile + " -T fields \
+                    -e ip.len -e ip.hdr_len -e ip.ttl \
+                    -e ip.proto -e tcp.srcport -e tcp.dstport -e tcp.seq \
+                    -e tcp.ack -e tcp.window_size_value -e tcp.hdr_len -e tcp.len \
+                    -e tcp.stream -e tcp.urgent_pointer \
+                    -e ip.flags -e ip.id -e ip.checksum -e tcp.flags -e tcp.checksum"
+
+    allFeatures = str(  os.popen(tsharkCommand).read()  )
+    allFeatures = allFeatures.replace('\t',',')
+    allFeaturesList = allFeatures.splitlines()
+    for features in allFeaturesList:
+        labelFeature.writelines(label + "," + features + "\n")
+##############################################################
